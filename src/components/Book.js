1b55dae85a420646589befbe5a364f022ff679f8
Pablo Garcia <pgarciacamou@gmail.com>
2017-09-23 14:04:33 -0700

    fix: add missing cover image
    based on feedback from an instructor at Udacity:
    
    Another thing you could do is create a default cover image to be displayed if a book doesn't have a cover because you could put text on the default cover that says "No Cover" (or think of something better) so that people will know that book doesn't have a cover rather than wondering if it just didn't download the cover image properly.
    
    You could test this out using this something like this as a default cover http://via.placeholder.com/128x193?text=No%20Cover (Use the URL in place of an image. You can customize the width, height, and text by changing the URL, but if you add very much text it will look too small at this image size.)
    
    Try searching for 'biography' to test this as the book "My First Life" by Brenda Dean Paul that is normally in the results does not have the imageLinks property.

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "../stylesheets/Book.css";
 import BookshelvesInfo from '../constants/BookshelvesInfo';
+import coverNotAvailable from '../assets/cover-not-available.jpg';
+import classNames from "classnames";
 
 function Book(book) {
   const { imageLinks, title, authors, onShelfChange, shelf } = book;
+  const noCoverImage = !imageLinks;
 
   return (
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{
-          backgroundImage: `url("${imageLinks ? imageLinks.thumbnail : ""}")`
-        }}></div>
+        <div
+          className={classNames("book-cover", { "no-cover": noCoverImage })}
+          style={{ backgroundImage: `url("${noCoverImage ? coverNotAvailable : imageLinks.thumbnail}")` }}
+        />
         <div className="book-shelf-changer">
           <select defaultValue={shelf || "none"} onChange={event => onShelfChange(book, event)}>
             <option value="none" disabled>Move to...</option>
